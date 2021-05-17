673876b462f44ef94ab6e3904957501a870aa673
Stoqn Minchev <s.minchev1@gmail.com>
2021-05-17 23:14:08 +0300

    Initial commit

---
+++ register.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+session_start();
+
+$username = $_POST ['username']; 
+$password = $_POST ['password'];
+$mysqli=mysqli_connect('127.0.0.1','root','','world'); 
+$pass     = $password;  
+$email    = $_POST ['email'];
+
+
+if(isset($_POST['submit']))
+{
+
+	$username = trim($_POST['username']);
+	$password = trim($_POST['password']);
+	$query = "SELECT username FROM users WHERE username='$username' AND password='$password'";
+	
+	$result = mysqli_query($mysqli,$query)or die(mysqli_error());
+	$num_row = mysqli_num_rows($result);
+	$row = mysqli_fetch_array($result);
+	
+		if( $num_row == 1 )
+	{
+		echo "Namerih :" . $username;
+	}
+	else
+	{
+		$insert = mysqli_query($mysqli, "INSERT INTO `users`(`username`, `password`, `email`) VALUES ('$username', '$password', '$email')");
+		
+	}
+	
+	echo '<pre>';
+	print_r($row);
+	echo '</pre>';
+	die();
+	
+	if( $num_row == 1 )
+	{
+		echo "Namerih :" . $username;
+	}
+	else
+	{
+		echo "Ne namiram :" . $username;
+	}
+}
